c557333b50fa3ce6b5754de52bfc1439f8339de6
mahmud aremu <aremumahmud2003@gmail.com>
2024-04-29 00:39:27 +0100

    uploaded files

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -18,21 +18,21 @@ function AboutUs({reverse , title , content , image  }){
     
     return (
         <div  className={reverse?"about":"about about_top"} style={reverse?{flexDirection:'row-reverse'}:{}}>
-           <div className={reverse?"ceo":'ceo_n'}>
+           <div className={reverse?"ceo":'ceo_n ceo'}>
             <img className={reverse?"shadow up":'up '}  src={image || jane} alt="" />
            { reverse && <div className="more">
             <div className="name name_">
-                <h1 className="up">Ethan Charles More</h1>
+                <h1 className="up">Ethan Charles Moore</h1>
                <p className="up">CEO of Supplify International</p>
             </div>
                <div className="sticky_ball1"></div>
                <div className="sticky_ball2"></div>
             </div>}
            </div>
-           <div style={reverse?{width:"100%"}:{}} className="section_About">
+           <div style={reverse?{width:"100%"}:{width:"100%"}} className="section_About">
             <h1 className="up">{title || 'About Supplify International .'}</h1>
 
-            { reverse && <> <br /> <img className='up' src={image} width={'50%'} style={{borderRadius:"10px"}} /> <br /> <br /></> }
+            { reverse && <>  <img  className='up display' src={image} width={'50%'} style={{borderRadius:"10px"}} /> </> }
            
             <p className='content_about'>{content || `At Supplify International, we are dedicated to being your premier destination for sourcing top-quality wholesale products from around the world. With a commitment to excellence, we offer a diverse range of products across various categories to meet the needs of businesses worldwide. Backed by a team of industry experts and driven by a passion for customer satisfaction, we strive to provide unparalleled service and value to our clients. Discover the difference with Supplify International and unlock new opportunities for your business.`}</p>
            </div>
